27456e5112c35e2b14c15ef396fed1b47c5d85ec
Dimon <Dimon999>
2023-06-29 15:12:52 +0300

    Changes not stop

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+"use strict"
+
 function receiveWord() {
     const  hiddenWord = prompt("Загадайте слово");
     const spliter = hiddenWord.split('');
@@ -16,23 +18,6 @@ function createCanvas() {
     canvasFrame.width = '800';
     canvasFrame.height = '300';
     document.querySelector('#image').appendChild(canvasFrame);
-}
-
-function makeGallowsFrame() {
-    let canvas = document.getElementById('canvasImg');
-    const ctx = canvas.getContext('2d');       
-    ctx.beginPath();
-    ctx.lineWidth = '5';
-    ctx.strokeStyle = 'blue';
-    ctx.moveTo(250, 2);
-    ctx.lineTo(250, 250);
-    ctx.stroke();
-    ctx.moveTo(250, 2);
-    ctx.lineTo(400, 2);
-    ctx.stroke();
-    ctx.moveTo(400, 2);
-    ctx.lineTo(400, 50);
-    ctx.stroke();
 }
 
 function createLetterCell(index) {
@@ -68,6 +53,96 @@ return spliter;
 
 let arrReceiveWord = startFilling();
 
+function  startGameLoop() {
+    let counter = 0;
+    
+      myLoop(counter);
+}
+
+function myLoop(count) {        
+    setTimeout(function() {  
+        let counter = count;
+        let winner = checkWin();  
+        if(winner == arrReceiveWord.length) {
+            alert("Вы выиграли");
+            return;
+        } 
+        while(counter < 5){ 
+            if(counter == 1) {
+                drowHead();
+            }
+            if(counter == 2) {
+                drowBody();
+            }
+            if(counter == 3) {
+                drowArms();
+            }
+            if(counter == 4) {
+                drowLegs();
+                setTimeout(function(){
+                    alert('Вы проиграли');
+                }, 500);
+                
+                return; 
+            }
+            setTimeout(function() {
+                let emptyCoincidences = checkCoincidences();
+                   if(emptyCoincidences.length == 0) {
+                    counter++;
+                   }
+                   if(emptyCoincidences.length > 0) {
+                    fillCellLetters(emptyCoincidences);
+                   }
+                    myLoop(counter);         
+            }, 1000);
+            break;            
+      }                       
+    }, 1500);
+  }
+
+function checkCoincidences(){  
+    let counterCoincidences = 0;
+    let inputLetter = prompt('Введите букву которая, как Вам кажется, есть в загаданном слове');
+    let arrayCoincidences = [];
+    for(let i = 0; i < arrReceiveWord.length; i++) {
+        if(arrReceiveWord[i].toLowerCase() === inputLetter.toLowerCase()) {
+            arrayCoincidences.push(i); 
+        }
+    }
+    return arrayCoincidences;
+}
+
+function fillCellLetters(arr) {
+    for(let i = 0; i < arr.length; i++) {
+        let cellPush = document.getElementById(`cell${arr[i]}`);
+        cellPush.innerHTML = `${arrReceiveWord[arr[i]]}`;
+        cellPush.classList.add('full');
+    }
+}
+
+function checkWin() {
+    let fullCell = document.getElementsByClassName('full');
+    return fullCell.length;
+
+}
+
+function makeGallowsFrame() {
+    let canvas = document.getElementById('canvasImg');
+    const ctx = canvas.getContext('2d');       
+    ctx.beginPath();
+    ctx.lineWidth = '5';
+    ctx.strokeStyle = 'blue';
+    ctx.moveTo(250, 2);
+    ctx.lineTo(250, 250);
+    ctx.stroke();
+    ctx.moveTo(250, 2);
+    ctx.lineTo(400, 2);
+    ctx.stroke();
+    ctx.moveTo(400, 2);
+    ctx.lineTo(400, 50);
+    ctx.stroke();
+}
+
 function drowHead() {
     const canvas = document.getElementById('canvasImg');
     const ctx = canvas.getContext('2d');
@@ -86,7 +161,7 @@ function drowBody() {
     ctx.lineWidth = '5';
     ctx.strokeStyle = 'blue';
     ctx.moveTo(400, 100);
-    ctx.lineTo(400, 175);
+    ctx.lineTo(400, 200);
     ctx.stroke();
 }
 
@@ -118,70 +193,4 @@ function drowLegs() {
     ctx.stroke();
 }
 
-function  startGameLoop() {
-    let counter = 0;
-    function myLoop() {        
-        setTimeout(function() {  
-            let winner = checkWin();  
-            if(winner == arrReceiveWord.length) {
-                alert("Вы выиграли");
-                return;
-            } 
-            while(counter < 5){ 
-                if(counter == 1) {
-                    drowHead();
-                }
-                if(counter == 2) {
-                    drowBody();
-                }
-                if(counter == 3) {
-                    drowArms();
-                }
-                if(counter == 4) {
-                    drowLegs();
-                    alert('Вы проиграли');
-                    return; 
-                }
-                
-               let emptyCoincidences = checkCoincidences();
-
-               if(emptyCoincidences.length == 0) {
-                counter++;
-               }
-               if(emptyCoincidences.length > 0) {
-                fillCellLetters(emptyCoincidences);
-               }
-                myLoop();  
-                break;   
-                      
-          }                       
-        }, 1500);
-      }
-      myLoop();
-}
-
-function checkCoincidences(){
-    counterCoincidences = 0;
-    let inputLetter = prompt('Введите букву которая, как Вам кажется, есть в загаданном слове');
-    let arrayCoincidences = [];
-    for(let i = 0; i < arrReceiveWord.length; i++) {
-        if(arrReceiveWord[i].toLowerCase() === inputLetter.toLowerCase()) {
-            arrayCoincidences.push(i); 
-        }
-    }
-    return arrayCoincidences;
-}
-
-function fillCellLetters(arr) {
-    for(let i = 0; i < arr.length; i++) {
-        let cellPush = document.getElementById(`cell${arr[i]}`);
-        cellPush.innerHTML = `${arrReceiveWord[arr[i]]}`;
-        cellPush.classList.add('full');
-    }
-}
-
-function checkWin() {
-    let fullCell = document.getElementsByClassName('full');
-    return fullCell.length;
-
-}
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
